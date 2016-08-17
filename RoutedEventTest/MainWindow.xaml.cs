ad8fa798eec80f9c4a8be4fda63b2eea4868e08e
Jon <Jon@github.com>
2016-08-17 02:36:53 -0500

    Initial Upload

---
+++ RoutedEventTest/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RoutedEventTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CustomEventLog(object sender, RoutedEventArgs e)
+        {
+            UIElement f = sender as UIElement;
+            string stringData = "Event: " + e.RoutedEvent + " | Source: " + e.Source + " | Sender: " + f.GetType();
+            this.UserCtrl.Output.Items.Add(stringData);
+        }
+
+        public void CustomCommandLog(object sender, ExecutedRoutedEventArgs e)
+        {
+            UIElement f = sender as UIElement;
+            string stringData = "Command: " + e.Command + " | Source: " + e.Source + " | Sender: " + f.GetType();
+            this.UserCtrl.Output.Items.Add(stringData);
+        }
+
+        public void LogCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        public void LogCanExecute2(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+    }
+
+    public class CustomCommands
+    {
+        public static readonly RoutedCommand LogCommand = new RoutedCommand("LogCommand", typeof(CustomCommands));
+        public static readonly RoutedCommand LogCommand2 = new RoutedCommand("LogCommand2", typeof(CustomCommands));
+    }
+}
+
---
+++ RoutedEventTest/UserControl1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RoutedEventTest
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            //this.CustomClick += StartCustomEvent;
+            //this.AddHandler(Button.ClickEvent, new RoutedEventHandler(StartCustomEvent));
+        }
+
+        public static readonly RoutedEvent CustomClickEvent = EventManager.RegisterRoutedEvent("CustomClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(UserControl1));
+
+        public event RoutedEventHandler CustomClick
+        {
+            add { AddHandler(CustomClickEvent, value); }
+            remove { AddHandler(CustomClickEvent, value); }
+        }
+
+        public void StartCustomEvent(object sender, RoutedEventArgs e)
+        {
+            (e.OriginalSource as UIElement).RaiseEvent(new RoutedEventArgs(CustomClickEvent));
+        }
+
+        public void EventLog(object sender, RoutedEventArgs e)
+        {
+            UIElement f = sender as UIElement;
+            string stringData = "#Event: " + e.RoutedEvent + " | Source: " + e.Source + " | Sender: " + f.GetType();
+            this.Output.Items.Add(stringData);
+        }
+
+        public void SuppressClick(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
